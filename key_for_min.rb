399cc7010e3266cc79ae92efff198d598ea2d91a
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-10 19:50:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  new_arr = []
+  name_hash.collect do |key, value|
+    new_arr << value
+    new_arr.sort
+    new_arr
 end
