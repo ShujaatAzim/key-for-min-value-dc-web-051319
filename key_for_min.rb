75be275ba2441596a4453ad6d374eb6a28b03725
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-10 20:18:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |key, value|
-    if name_hash[min_key].nil? || name_hash[min_key] > value
+    if min_key.nil? || name_hash[min_key] > value
       min_key = key
     end
   end
